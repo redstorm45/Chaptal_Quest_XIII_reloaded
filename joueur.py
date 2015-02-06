8dcffb12a0d1be8d91856c9de4ab17f56a2abc17
husgarl <charrier.quentin@yahoo.fr>
2015-02-06 16:38:31 +0100

    modif joueur

--- joueur.py
+++ joueur.py
@@ -9,6 +9,9 @@ class Joueur:
         self.hitbox     = [ -1/4 , 1/4 , -1/4 , 1/4 ]  # xmin , xmax , ymin , ymax
         self.spriteName = "gobelin"
         self.direction  = 1
+        self.lvl = 1
+        self.hp = 100 * self.lvl
+        self.dammage = 5
     
     #mouvement de x cases vers la droite
     # et de y cases vers le bas
