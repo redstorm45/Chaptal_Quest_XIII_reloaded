8918ed3f300c7d6700411e3b45b4c60a81a240dc
redstorm45 <pierre.cadart@gmail.com>
2015-02-06 17:05:43 +0100

    more walls
    +angles exterieurs des murs

--- dessin.py
+++ dessin.py
@@ -23,16 +23,20 @@ def loadAllSprites():
     workDir = os.getcwd()
     os.chdir(workDir+"/sprites")
     sprites["mur"]={}
-    sprites["mur"]["H"]  = pygame.image.load("mur_haut.bmp")
-    sprites["mur"]["HG"] = pygame.image.load("mur_angle_gauche_haut.bmp")
-    sprites["mur"]["G"]  = pygame.image.load("mur_gauche.bmp")
-    sprites["mur"]["BG"] = pygame.image.load("mur_angle_gauche_bas.bmp")
-    sprites["mur"]["B"]  = pygame.image.load("mur_bas.bmp")
-    sprites["mur"]["BD"] = pygame.image.load("mur_angle_droite_bas.bmp")
-    sprites["mur"]["D"]  = pygame.image.load("mur_droite.bmp")
-    sprites["mur"]["HD"] = pygame.image.load("mur_angle_droite_haut.bmp")
-    sprites["plancher"]  = pygame.image.load("plancher.bmp")
-    sprites["joueur"]    = pygame.image.load("perso.png")
+    sprites["mur"]["H"]   = pygame.image.load("mur_haut.bmp")
+    sprites["mur"]["HG"]  = pygame.image.load("mur_angle_gauche_haut.bmp")
+    sprites["mur"]["HG2"] = pygame.image.load("mur_angle2_gauche_haut.bmp")
+    sprites["mur"]["G"]   = pygame.image.load("mur_gauche.bmp")
+    sprites["mur"]["BG"]  = pygame.image.load("mur_angle_gauche_bas.bmp")
+    sprites["mur"]["BG2"] = pygame.image.load("mur_angle2_gauche_bas.bmp")
+    sprites["mur"]["B"]   = pygame.image.load("mur_bas.bmp")
+    sprites["mur"]["BD"]  = pygame.image.load("mur_angle_droite_bas.bmp")
+    sprites["mur"]["BD2"] = pygame.image.load("mur_angle2_droite_bas.bmp")
+    sprites["mur"]["D"]   = pygame.image.load("mur_droite.bmp")
+    sprites["mur"]["HD"]  = pygame.image.load("mur_angle_droite_haut.bmp")
+    sprites["mur"]["HD2"] = pygame.image.load("mur_angle2_droite_haut.bmp")
+    sprites["plancher"]   = pygame.image.load("plancher.bmp")
+    sprites["joueur"]     = pygame.image.load("perso.png")
     
     loadAnimSprite( "gobelin" )
     
@@ -72,23 +76,31 @@ def drawCase(fenetre,region,x,y):
     yEcran = y * SPRITE_SIZE  + yOffset
     
     if region.at(x,y) == 1:
-        fenetre.blit(sprites["plancher"] , (xEcran,yEcran))
+        fenetre.blit(sprites["plancher"]   , (xEcran,yEcran))
     elif region.at(x,y) == 2:
-        fenetre.blit(sprites["mur"]["H"] , (xEcran,yEcran))
+        fenetre.blit(sprites["mur"]["H"]   , (xEcran,yEcran))
     elif region.at(x,y) == 3:
-        fenetre.blit(sprites["mur"]["HG"], (xEcran,yEcran))
+        fenetre.blit(sprites["mur"]["HG"]  , (xEcran,yEcran))
     elif region.at(x,y) == 4:
-        fenetre.blit(sprites["mur"]["G"] , (xEcran,yEcran))
+        fenetre.blit(sprites["mur"]["G"]   , (xEcran,yEcran))
     elif region.at(x,y) == 5:
-        fenetre.blit(sprites["mur"]["BG"], (xEcran,yEcran))
+        fenetre.blit(sprites["mur"]["BG"]  , (xEcran,yEcran))
     elif region.at(x,y) == 6:
-        fenetre.blit(sprites["mur"]["B"] , (xEcran,yEcran))
+        fenetre.blit(sprites["mur"]["B"]   , (xEcran,yEcran))
     elif region.at(x,y) == 7:
-        fenetre.blit(sprites["mur"]["BD"], (xEcran,yEcran))
+        fenetre.blit(sprites["mur"]["BD"]  , (xEcran,yEcran))
     elif region.at(x,y) == 8:
-        fenetre.blit(sprites["mur"]["D"] , (xEcran,yEcran))
+        fenetre.blit(sprites["mur"]["D"]   , (xEcran,yEcran))
     elif region.at(x,y) == 9:
-        fenetre.blit(sprites["mur"]["HD"], (xEcran,yEcran))
+        fenetre.blit(sprites["mur"]["HD"]  , (xEcran,yEcran))
+    elif region.at(x,y) == 10:
+        fenetre.blit(sprites["mur"]["HG2"] , (xEcran,yEcran))
+    elif region.at(x,y) == 11:
+        fenetre.blit(sprites["mur"]["BG2"] , (xEcran,yEcran))
+    elif region.at(x,y) == 12:
+        fenetre.blit(sprites["mur"]["BD2"] , (xEcran,yEcran))
+    elif region.at(x,y) == 13:
+        fenetre.blit(sprites["mur"]["HD2"] , (xEcran,yEcran))
 
 def drawPlayer(fenetre,player):
     x,y = player.position[1] , player.position[2]
