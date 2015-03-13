ac50ba19649b0cf9e5c16e3f308ac85ced49fa2c
husgarl <charrier.quentin@yahoo.fr>
2015-03-13 16:40:11 +0100

    debut module PNG

---
+++ PNG.py
@@ -0,0 +1,7 @@
+
+class PNG():
+    def __init__(self,name,position):
+        
+        file = open( "PNG/" + name + ".txt")
+        self.name = file.readline().strip().split(";")
+        self.spriteName  = file.readline().strip().split(";")
